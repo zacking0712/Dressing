57d293bdf9f5078fb3675874cbce190b67f8e4fa
zacking0712 <nldangkhoa0712@gmail.com>
2024-03-04 09:54:38 +0700

    Hoàn thành bài tập

--- controllers/ListItem.js
+++ controllers/ListItem.js
@@ -1,23 +1,51 @@
 
 // Khởi tạo thẻ img
 
-let dressUp = document.createElement('img')
-let divContain = document.querySelector('.contain')
-divContain.appendChild(dressUp)
-dressUp.id = "dressUp"
+// let dressUp = document.createElement('img')
+// let divContain = document.querySelector('.contain')
+// divContain.appendChild(dressUp)
+// dressUp.id = "dressUp"
 
-// function thử đồ
-function thuDo(e) {
-    let clothes = e.target.id
-    console.log(clothes);
-    let img = document.getElementById("dressUp")
-    img.src = clothes
-    img.style.position = "absolute"
-    img.style.top = "-30%"
-    img.style.left = "-5%"
-    img.style.zIndex = "10"
-    img.style.transform = "scale(0.5)"
+// // function thử đồ
+// function thuDo(e) {
+//     let clothes = e
+//     console.log(clothes);
+//     let img = document.getElementById("dressUp")
+//     img.src = clothes
+//     img.style.position = "absolute"
+//     img.style.top = "-30%"
+//     img.style.left = "-5%"
+//     img.style.zIndex = "10"
+//     img.style.transform = "scale(0.5)"
+// }
+let imgID = document.querySelectorAll(".contain div img")
+console.log(imgID)
+
+function thuDo(url, name) {
+    console.log(url)
+    console.log(name);
+    // let stringName = name.includes(imgID[2].id)
+    // for(let i = 0; i < imgID.length)
+    // console.log(name)
+    // console.log(typeof imgID[2].id)
+    // console.log(stringName)
+    for (let i = 0; i < imgID.length; i++) {
+        let stringName = name.includes(imgID[i].id)
+        console.log(imgID[i])
+        console.log()
+        if(stringName == true) {
+            if(imgID[i] === "Background") {
+                document.querySelector(".background").style.backgroundImage = `url(${url})`
+                // document.getElementById(imgID[i].id).style.height = "1500px !important"
+            } else {
+                document.getElementById(imgID[i].id).style.width = "100%"
+                document.getElementById(imgID[i].id).src = url
+            }
+        }
+    }
 }
+
+
 
 function getValueData() {
     let promise = axios({
@@ -49,7 +77,7 @@ function getValueData() {
                         <div class="float-left w-25 text-center" style="padding: 20px">
                             <img class="rounded" src="${item.imgSrc_jpg}" alt="">
                             <p class="mt-3 font-weight-bold">${item.name}</p>
-                            <button class="btn btn-dark" id="${item.imgSrc_png}" onclick="thuDo(event)">Thử đồ</button>
+                            <button class="btn btn-dark" onclick="thuDo('${item.imgSrc_png}', '${item.name}')">Thử đồ</button>
                         </div>
                     `
                 }
